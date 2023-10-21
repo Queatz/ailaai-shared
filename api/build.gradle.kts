0ad600e718fe6db5193ea7dc133cba783c18c704
Jacob Ferrero <jacobaferrero@gmail.com>
2023-10-22 01:55:38 +0700

    Use API from shared library

---
+++ api/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("multiplatform") version "1.9.10"
+    kotlin("plugin.serialization") version "1.9.10"
+}
+
+group = "app.ailaai.shared"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(17)
+        withJava()
+    }
+    js {
+        browser {
+
+        }
+    }
+//    val hostOs = System.getProperty("os.name")
+//    val isArm64 = System.getProperty("os.arch") == "aarch64"
+//    val isMingwX64 = hostOs.startsWith("Windows")
+//    val nativeTarget = when {
+//        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+//        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+//        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+//        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+//        isMingwX64 -> mingwX64("native")
+//        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+//    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                val ktorVersion = "2.3.4"
+                implementation(project(":models"))
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.5.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+            }
+        }
+        val jvmMain by getting
+        val jsMain by getting
+//        val nativeMain by getting
+    }
+}
